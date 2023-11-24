514dd667e947d0d47f61637f3ee66d245f43e181
zafar2006 <kasimovzafar683@gmail.com>
2023-11-24 15:37:55 +0600

    first commit

---
+++ dart_application_9.dart
@@ -0,0 +1,125 @@
+// import 'dart:io';
+
+// import 'dart:html';
+// import 'dart:io';
+
+void main(List<String> arguments) {
+//1)ЗАДАЧА
+  //   for (int i = 10; i <= 20; i++) {
+  //     print(i * i);
+  //   }
+
+//2)ЗАДАЧА
+  // print('Введите число');
+  // int n = int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for (int i = 1; i <= n; i++) {
+  //   summ += i;
+  // }
+  // print('Сумма числа от 1 до $n ровна: $summ');
+
+//3)ЗАДАЧА
+  // print('Введите сумму');
+  // double s = double.parse(stdin.readLineSync()!);
+  // print('Введите срок');
+  // int d = int.parse(stdin.readLineSync()!);
+  // for (int i = 1; i <= d; i++) {
+  //   s = s + (s * 0.03);
+  //   print('$i месяц $s');
+  // }
+
+//4)ЗАДАЧА
+  // for (int i = 20; i <= 50; i++) {
+  //   if (i % 3 == 0 && i % 5 != 0) {
+  //     print(i);
+  //   }
+  // }
+
+//5)ЗАДАЧА
+  // int summ = 0;
+  // for (int i = 1; i <= 50; i++) {
+  //   if (i % 5 == 0 || i % 7 == 0) {
+  //     summ += i;
+  //     print(i);
+  //     print(summ);
+  //   }
+  // }
+
+//6)ЗАДАЧА
+  // for (int i = 10; i <= 99; i++) {
+  //   if (i % 4 == 0 && i % 6 != 0) {
+  //     print(i);
+  //   }
+  // }
+
+//7)ЗАДАЧА
+  // int summ = 0;
+  // for (int i = 100; i <= 200; i++) {
+  //   if (i % 17 == 0) {
+  //     summ += i;
+  //   }
+  // }
+  // print(summ);
+
+//8)ЗАДАЧА
+  // print('Введите число');
+  // int r int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for(int i = 1; i<= r; i++){
+  //   summ =summ + ( i *1);
+  //   print(i);
+  //   print()
+  // }
+  // print(summ)
+//9)ЗАДАЧА
+
+  // print('Введите начало');
+  // int a = int.parse(stdin.readLineSync()!);
+  // print('Ввкдите второе число');
+  // int b = int.parse(stdin.readLineSync()!);
+  // int summ = 0;
+  // for (int i = a; i <= b; i++) {
+  //   summ += i;
+  // }
+  // print(summ);
+
+//10)ЗАДАЧА
+  // print('Введите год');
+  // int year = int.parse(stdin.readLineSync()!);
+  // if (year % 4 == 0) {
+  //   print('Высокосный');
+  // } else {
+  //   print('нет');
+  // }
+
+//11)ЗАДАЧА
+  // print('Введите число 1');
+  // int a = int.parse(stdin.readLineSync()!);
+  // print('Введите число 2');
+  // int b = int.parse(stdin.readLineSync()!);
+  // for (int i = a; i > -1; i++) {
+  //   if (i % a == 0 && i % b == 0) {
+  //     print(i);
+  //     break;
+  //   }
+  // }
+
+//12)ЗАДАЧА
+  // int a = 123;
+  // String b = a.toString();
+  // int summ = 0;
+  // for (int i = 0; i < b.length; i++) {
+  //   int.parse(b[i]);
+  //   summ += int.parse(b[i]);
+  // }
+  // print(summ);
+
+//13)ЗАДАЧА
+  // String a = 'qwq';
+  // String b = a.split('').reversed.toList().join();
+  // if (a == b) {
+  //   print('Палендром');
+  // } else {
+  //   print('Не палендром');
+  // }
+}
